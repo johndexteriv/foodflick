1ccad59d37baa580120b11ef57ea8b0c55c75357
John Garner <jdextergarneriv@gmail.com>
2020-10-13 22:10:06 -0400

    Created on click event to get checkbox value and started function to query random recipe api

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-var slider = document.getElementById("slider");
 
-noUiSlider.create(slider, {
-  start: [5, 8],
-  connect: true,
-  range: {
-    min: 0,
-    max: 10,
-  },
-});
+var foodsSelected = [];
+console.log(foodsSelected);
+
+$('#submit').on("click", function(){
+	
+
+	console.log('this is the button', $(".foodcheck:checked"));
+
+	$(".foodcheck:checked").each(function(){
+		console.log($(this).val());
+		(foodsSelected).push($(this).val())
+
+	})
+
+})
+
+function food() {
+
+	var apiKey = "cc184da5eab54e0cab6a17a989d5fa0c"
+
+	var queryURL = "https://api.spoonacular.com/recipes/complexSearch?apiKey=" + apiKey + "&sort=random&includeIngredients=milk,eggs" 
+
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}) .then (function(response){
+		console.log('this is the reponse', response)
+	})
+
+};
